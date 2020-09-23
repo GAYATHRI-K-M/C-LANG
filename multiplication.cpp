da1caab91884305a428748147489b756ef2408c1
Gayathri <70088536+GAYATHRI-K-M@users.noreply.github.com>
2020-09-23 18:46:03 +0530

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, product;
+
+cout <<"enter two numbers: " <<"\n";
+cin >> a >> b;
+product = a * b;
+cout << a << "*" << b << "=" << product;
+return 0;
+
+}
---
+++ size.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int integertype;
+	char chartype;
+	float floattype;
+	double doubletype;
+	
+	//calculate the size of int
+	cout<<"the size if int is: " <<sizeof integertype <<"\n";
+	
+	//calculate the size of char
+	cout<<"the size of char is: " <<sizeof chartype <<"\n";
+	
+    //calculate the size of float
+	cout<<"the size of float is: " <<sizeof floattype <<"\n";
+	
+	
+	//calculate the size of double
+	cout<<"the size of double is: " <<sizeof doubletype <<"\n";
+	
+	return 0;
+	}
+
